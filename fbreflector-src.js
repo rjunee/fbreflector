e7d4a07b5b3d9945cc0add463b92e84caedd19cc
Ryan Junee <ryan@junee.org>
2010-08-12 23:37:06 -0700

    Add github URL to comment

--- fbreflector-src.js
+++ fbreflector-src.js
@@ -1,6 +1,7 @@
 /**
  * Copyright Ryan Junee
- *
+ * http://github.com/rjunee/fbreflector
+ * 
  * Licensed under the MIT License (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,9 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- *
- * This code loads the specified user's facebook feed and builds HTML to display it
  *
  */
  
